2688e9160a292dd65aaebeb94f47408d0d332c68
Okoye Peter <okoyepeter039@gmail.com>
2026-03-18 07:15:32 +0100

    chore: Disable authentication middleware for log routes.

--- src/routes/log.routes.ts
+++ src/routes/log.routes.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 // Assuming only authenticated users can view/clear logs, and ideally, only admins.
 // For now, protecting it simply:
-router.use(protect);
+// router.use(protect);
 
 router.get('/', getLogs);
 router.get('/paga', getPagaLogs);
