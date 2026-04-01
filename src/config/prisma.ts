13a2068b76786205a12062cd052b6d82c5a0c46b
Okoye Peter <okoyepeter039@gmail.com>
2026-04-01 21:15:07 +0100

    refactor: revert custom prisma client output path to use default @prisma/client package

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '../generated/prisma/index.js';
+// import { PrismaClient } from '../generated/prisma/index.js';
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
