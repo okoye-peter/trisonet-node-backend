b260dac91f17dadab00824ebf1a01cf4252e5416
Okoye Peter <okoyepeter039@gmail.com>
2026-04-03 18:42:21 +0100

    refactor: revert Prisma client output path to default @prisma/client location

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma, WalletType, User, LoanStatus, GuardianSlotType } from '../generated/prisma';
+import { PrismaClient, Prisma, WalletType, User, LoanStatus, GuardianSlotType } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
