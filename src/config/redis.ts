05241c3e431fc51c5f626cf2d7c255c50a1fd007
Okoye Peter <okoyepeter039@gmail.com>
2026-04-03 17:21:01 +0100

    refactor: update redis configuration to use connection URL instead of host and port

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,8 @@
 
 export const redisConnection = {
-    host: process.env.REDIS_HOST || 'localhost',
-    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    url: process.env.REDIS_URL || 'redis://localhost:6379',
+    // host: process.env.REDIS_HOST || 'localhost',
+    // port: parseInt(process.env.REDIS_PORT || '6379', 10),
     // password: process.env.REDIS_PASSWORD || undefined,
     maxRetriesPerRequest: null,
 };
